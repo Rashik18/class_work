56d14cd7a3550114fc4aa620a3f965a13bbc1593
Rashik18 <rashikzaman498@gmail.com>
2019-01-09 01:30:09 +0000

    Question has been added

--- computational_1-1.py
+++ computational_1-1.py
@@ -1,3 +1,16 @@
+'''For a satellite, orbiting around the earth with a time period, T,
+we know that
+h =((GMT^2/4(pi)^2)^(1/3))-R
+
+Where G = 6.67x10e-11
+M = 5.97x1024kg and
+
+R = 6371km
+I Write a program to calculate the altitude of a satellite for the
+input time period.'''
+
+
+
 G = 6.673*10**-11
 M = 5.97*10**24
 R = 6371
